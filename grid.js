e5a088e2be1cfa72bada2e7207331e1850207217
Alice Rosier <alicetlrosier@gmail.com>
2016-11-23 20:49:34 +1300

    updated to make grid makes sounds

--- grid.js
+++ grid.js
@@ -1,3 +1,35 @@
+
+document.addEventListener('DOMContentLoaded', setEventListeners);
+
+function setEventListeners() {
+  //1. get all of the elements with class grid
+  var gridElements = document.getElementsByClassName('grid');
+  //2. for each element add a mouseover handler
+  for(var i = 0; i < gridElements.length; i++){
+     gridElements[i].addEventListener('mouseover', playSound);
+     //gridElements[i].addEventListener('mouseleave', pauseSound)
+  }
+
+  function playSound(event){
+     console.log(event);
+     //event.target is the element where the event was triggered from (e.g. the div that was clicked)
+     var sourceDiv = event.target;
+     //children returns an array of child elements from the sourceDiv
+     sourceDiv.children[0].play();
+  }
+
+  function pauseSound(event){
+     console.log(event);
+     //event.target is the element where the event was triggered from (e.g. the div that was clicked)
+     var sourceDiv = event.target;
+     //children returns an array of child elements from the sourceDiv
+     sourceDiv.children[0].pause();
+  }
+
+}
+
+
+/*
 document.addEventListener('DOMContentLoaded', playSound)
 
 
@@ -36,10 +68,10 @@ var hoverOver = gridNum.length;
 };
 
 
-function chooseAudioSound(){
+function chooseAudioSound(){///here is where it is established which audio sound to request, depending on what grid is being hovered over...
   for (var i = 0; i < hoverOver; i++){
-   if ('grid-1' == true) {
-     var audioSounds1
+   if ('grid-1' == true) { //if it is grid-1
+     var audioSounds1 //then play this sound bite
    }
      else if ('grid-2' == true) {
      audioSounds2("2")
@@ -64,16 +96,9 @@ var audioSounds = {
  1:"127788__149203__key-01-16bit.wav",
  2:"316901__jaz-the-man-2__do-octave.wav",
  3:"363161__sirkoto51__toy-piano-key-g.wav",
- 4:"316901__jaz-the-man-2__do-octave.wav"
- 5:"316912__jaz-the-man-2__sol.wav"
+ 4:"316901__jaz-the-man-2__do-octave.wav",
+ 5:"316912__jaz-the-man-2__sol.wav",
  6:"363161__sirkoto51__toy-piano-key-g.wav"
-
 }
-
-;
-var audioSounds2 = ("");
-var audioSounds3 = ("");
-var audioSounds4 = ("");
-var audioSounds5 = ("");
-var audioSounds6 = ("");
 }
+*/
